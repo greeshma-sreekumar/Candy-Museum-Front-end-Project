403efaab1cfbb15fc8c2db8bce2f203f000e5b18
GREESHMA SREEKUMAR <61485318+greeshma-sreekumar@users.noreply.github.com>
2022-05-08 13:35:10 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = new express();
-const path = require('path');
-app.use(express.static('public'));
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname+"/home.html");
-})
-app.listen(3000, ()=> console.log("server is listening..."))
